17ad22544e974eab209ac3e61aa0cdc7cafba8b4
descent <descent@weiya.com.tw>
2012-09-07 13:49:34 +0800

    add some code to test class Node

--- list/main.cpp
+++ list/main.cpp
@@ -16,12 +16,11 @@ void print(Node *n)
   cout << endl;
 }
 
-int main()
+Node *create_list()
 {
   int data[] = {1,2,3,4,5};
   Node *head = new Node(0);
   Node *cur_n = head;
-
   for (int i=0 ; i < sizeof(data)/sizeof(int) ; ++i)
   {
     Node *n = new Node(data[i]);
@@ -29,6 +28,17 @@ int main()
     cur_n = n;
   }
   cur_n->set_next(0);
+  return head; 
+}
+
+int main()
+{
+  Node *head = create_list();
+
+  print(head);
+  head->del_next_node();
+  print(head);
+  head->add_next_node(new Node(11));
   print(head);
 
   return 0;
