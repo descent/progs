afa0063bdcd22d2c5d7734c68e16245a144e0eb8
descent <descent@weiya.com.tw>
2012-09-13 15:18:43 +0800

    modified:   main.c     print can scroll line.     print (x, y) informatin.

--- console/main.c
+++ console/main.c
@@ -5,16 +5,24 @@
 void init_buf(Console *console)
 {
   for (int i=0 ; i < PAGE*BUF_H ; ++i)
-    console->buf[i] = 'z';
+    for (int j=0 ; j < BUF_W ; ++j)
+      console->buf[j][i] = 'z';
 }
 
 void print_screen(Console *console)
 {
-  int index = console->begin;
+  //int index = console->begin;
+  int index = console->scroll_h;
+
+
   for (int i=0 ; i < BUF_H ; ++i)
   {
-    printf("%c ", console->buf[index]);
+    for (int j=0 ; j < BUF_W ; ++j)
+    {
+      printf("%c ", console->buf[j][index]);
+    }
     index = (index+1) % (PAGE*BUF_H);
+    printf("\n");
   }
   printf("\n");
 }
@@ -22,7 +30,13 @@ void print_screen(Console *console)
 void print_buf(Console *console)
 {
   for (int i=0 ; i < PAGE*BUF_H ; ++i)
-    printf("%c ", console->buf[i]);
+  {
+    for (int j=0 ; j < BUF_W ; ++j)
+    {
+      printf("%c ", console->buf[j][i]);
+    }
+    printf("\n");
+  }
   printf("\n");
 }
 
@@ -32,20 +46,59 @@ int main(int argc, const char *argv[])
   console.begin = 0;
 
   init_buf(&console);
+  printf("init buf:\n");
   print_buf(&console);
   int y = 0;
   char ch = '0';
-  for (y=0 ; y < 50; ++y)
+  int warp = 0;
+  for (y=0 ; y < 20; ++y)
   {
-    put(&console, y, ch);
-    printf("y: %d\t\t", y);
-    printf("input: %c\t\t", ch);
-    printf("begin: %d\n", console.begin);
-    print_buf(&console);
-    print_screen(&console);
+    for (int x=0 ; x < BUF_W; ++x)
+    {
+      put_ch(&console, x, y, ch);
+      printf("(%d, %d)\t\t", x, y);
+      printf("input: %c\t\t", ch);
+      printf("begin: %d\t\t", console.begin);
+      if (y >= PAGE*BUF_H)
+      {
+        //printf("warp!!\n");
+        warp = 1;
+      }
+      printf("scroll: ");
+#if 1
+      if (warp == 1)
+      {
+        int index_y = (console.begin + BUF_H) % (PAGE*BUF_H);
+
+        //for (int i = 0 ; i < (PAGE-1)*BUF_H ; ++i)
+        // begin 往下加上 BUF_H, 再往下加 (PAGE-1)*BUF_H 個 (一次加一)
+        // ex: begin = 5, (5+3) % 9 = 8
+        // 8, 9%9 = 0, 10%9 = 1, 11%9 = 2 , 12%9 = 3 , 13%9 = 4
+        for (int i = 0 ; i < (PAGE-1)*BUF_H ; ++i)
+        {
+          printf("%d ", (index_y+ (PAGE-1)*BUF_H - 1- i)%(PAGE*BUF_H) );
+        }
+      }
+      else
+#endif
+      {
+        for (int i = console.begin-1 ; i >= 0 ; --i)
+          printf("%d ", i);
+      }
+      printf("\n");
+    }
     ++ch;
     if (ch==':') 
       ch = '0';
+    printf("print buf:\n");
+    print_buf(&console);
+
+    printf("print screen (%dX%d):\n", BUF_W, BUF_H);
+    print_screen(&console);
+
+    printf("scroll screen\n");
+    scroll(&console, -1);
+    print_screen(&console);
   }
 
   return 0;
