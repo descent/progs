7129648431962e79d0732c42b6f5f67cf3aed97d
descent <descent@weiya.com.tw>
2012-09-13 15:17:46 +0800

    modified:   console.c modified:   console.h     add scroll()     add x direction.

--- console/console.c
+++ console/console.c
@@ -1,10 +1,37 @@
 #include "console.h"
 
-int put(Console *console, int y, char ch)
+int init(Console *console)
 {
-  console->buf[y%(PAGE*BUF_H)] = ch;
+  console->warp = 0;
+}
+
+int put_ch(Console *console, int x, int y, char ch)
+{
+  if (y >= PAGE*BUF_H)
+    console->warp = 1;
+  console->buf[x][y%(PAGE*BUF_H)] = ch;
   if (y <= (BUF_H-1))
     console->begin = 0;
   else
     console->begin = (y - (BUF_H-1))%(PAGE*BUF_H);
+  console->scroll_h = console->begin;
+}
+
+// h < 0 scroll up 
+// h > 0 scroll down
+int scroll(Console *console, int h)
+{
+  int scroll_h;
+  if (h < 0)
+  {
+    scroll_h = -h;
+  }
+
+  if ((console->scroll_h + h) > console->begin)
+    return -1;
+
+  if (console->begin >= 1)
+  {
+    console->scroll_h = console->begin + h;
+  }
 }
--- console/console.h
+++ console/console.h
@@ -1,16 +1,20 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
-#define BUF_H 5
+#define BUF_W 2
+#define BUF_H 3
 #define PAGE 3
 
 
 typedef struct Console_
 {
-  char buf[PAGE*BUF_H];
+  char buf[BUF_W][PAGE*BUF_H];
   int begin;
+  int scroll_h;
+  int warp;
 }Console;
 
-int put(Console *console, int y, char ch);
+int put_ch(Console *console, int x, int y, char ch);
+int scroll(Console *console, int h);
 
 #endif
