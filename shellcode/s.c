70695ab74addf3231bf9b449b465f678e98fabd7
descent <descent@weiya.com.tw>
2012-11-19 11:57:12 +0800

    new file:   shellcode/makefile new file:   shellcode/s.c

---
+++ shellcode/s.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#if 0
+   0:   b8 01 00 00 00          mov    $0x1,%eax
+   5:   bb 00 00 00 00          mov    $0x0,%ebx
+   a:   cd 80                   int    $0x80
+
+#endif
+char __attribute__ ((section (".text"))) shellcode[] = "\xb8\x01\x00\x00\x00\xbb\x00\x00\x00\x00\xcd\x80";
+typedef void (*FUNCPTR)();
+
+int main() {
+    printf("<< Shellcode begin >>\n");
+
+    FUNCPTR fp = (FUNCPTR)shellcode;
+    fp();
+}
