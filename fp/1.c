5bb823f24b04d904beabef13a6b8dc30ac0463ad
descent <descent@weiya.com.tw>
2013-01-16 16:31:30 +0800

    new file:   fp/1.c new file:   fp/2.c new file:   fp/readme

---
+++ fp/1.c
@@ -0,0 +1,8 @@
+int main(int argc, const char *argv[])
+{
+//int newdef(float d, char i);
+  float d = 10.0;
+  char j = 3;
+  newdef(d, j);        
+  return 0;
+}
---
+++ fp/2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int newdef(float d, char i)
+{
+  printf("newdef f = %f, char = %x\n", d, i);
+}
