b35620a0af7640db4a8606aa35d3ba9c07169544
descent <descent@weiya.com.tw>
2013-04-26 14:56:11 +0800

    new file:   recursive/makefile 	new file:   recursive/sum.c

---
+++ recursive/sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int sum(int n)
+{
+  if (n==0)
+    return 0;
+  else
+    return n+sum(n-1);
+}
+
+int main(int argc, char *argv[])
+{
+  printf("sum: %d\n", sum(5)); 
+  return 0;
+}
