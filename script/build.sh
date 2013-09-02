583a625099ce07bb656721703269056417ef7d2e
descent <descent@weiya.com.tw>
2013-09-02 14:56:18 +0800

    add watch option to enable/disable watchdog

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,17 @@
 # auto git clone and make the code
 #!/bin/sh
+
+en_wd='0'
+if [ "$1" != "" ]; then
+  en_wd='1'
+else
+  echo "watch dog support current disable"
+  echo "========================"
+  echo "$0 1 can enable watch dog support"
+fi
+echo "enable watch dog: $en_wd"
+sleep 7
+
 qt_make=/home/lucid/ltib/rootfs/usr/local/Trolltech/qt4/bin/qmake
 
 cd /tmp
@@ -44,21 +56,35 @@ $qt_make qtes.pro
 make
 
 cd /tmp/mmadplayer
+$qt_make prog.pro
+make
+
 ln -s /tmp/update_connect_status/inc/func.h /tmp/mmadplayer/func.h
 version=`git show --summary |grep commit |cut -d ' ' -f2`
 sed -i -e "s/\$Format:%H\\$/$version/g" const.h
+
+
+if [ "$en_wd" = "1" ]; then
+  echo "DEFINES += ENABLE_WATCHDOG" >> mmadplayer.pro
+fi
 $qt_make mmadplayer.pro
 make
 make install
 
+
 mkdir /tmp/pkg/
 
+
 cp -r /tmp/mmadplayer/mmadplayer_dir/  /tmp/pkg
+
+cp /tmp/mmadplayer/*.qm /tmp/pkg/mmadplayer_dir/
+cp /tmp/mmadplayer/wd_tool /tmp/pkg/mmadplayer_dir/
+
+
 cp /tmp/update_connect_status/tools/*  /tmp/pkg/mmadplayer_dir/
 cp -r /tmp/c2d_rect/c2d_rect  /tmp/pkg/mmadplayer_dir/
 cp -r /tmp/c2d_ticker/c2d_ticker  /tmp/pkg/mmadplayer_dir/
 mv /tmp/pkg/mmadplayer_dir/reg* /tmp/pkg/
-cp /tmp/pkg/mmadplayer/*.qm /tmp/pkg/mmadplayer_dir/
 sed -i -e "s/mmadplayer&/mmadplayer -l jp\\&/g" /tmp/pkg/mmadplayer_dir/mmadplayer_v2.sh
 cp /tmp/pkg/mmadplayer_dir/mmadplayer_v2.sh /tmp/pkg/mmadplayer_dir/mmadplayer.sh
 mv /tmp/pkg/mmadplayer_dir /tmp/pkg/mmadplayer
