c4ec6abf13142e89c8034ce9a2d9912bc2c6e5f1
descent <descent@weiya.com.tw>
2013-11-14 09:46:39 +0800

    test child QWidget will be call dtor

--- qt_thread1/w.cpp
+++ qt_thread1/w.cpp
@@ -1,3 +1,31 @@
 #include "w.h"
 
+Widget::Widget():m_label(new QLabel), m_button(new QPushButton("Button")), m_worker(new Worker) 
+{ 
+  button_ = new QPushButton("Button2");  
+  new MyWidget(this);
+  new MyWidget(this);
 
+        QVBoxLayout * vbox = new QVBoxLayout(this); 
+        vbox->addWidget(m_label); 
+        vbox->addWidget(m_button); 
+        vbox->addWidget(button_); 
+        setLayout(vbox); 
+        thread = new QThread(this); 
+        m_worker->moveToThread(thread); 
+         connect(m_button,SIGNAL(clicked()),this,SLOT(onButtonClicked())); 
+        connect(this,SIGNAL(clicked(int)),m_worker,SLOT(slot1(int))); 
+        connect(m_worker,SIGNAL(sig1(QString)),m_label,SLOT(setText(QString))); 
+        thread->start(); 
+} 
+
+
+MyWidget::MyWidget ( QWidget * parent, Qt::WindowFlags f)
+  : QWidget(parent, f)
+{
+}
+
+MyWidget::~MyWidget()
+{
+  qDebug() << "MyWidget::~MyWidget()";
+}
--- qt_thread1/w.h
+++ qt_thread1/w.h
@@ -18,30 +18,31 @@ signals:
     void sig1(const QString& t); 
 };
 
+class MyWidget:public QWidget 
+{
+  Q_OBJECT 
+
+  public: 
+    MyWidget ( QWidget * parent = 0, Qt::WindowFlags f = 0 );
+    ~MyWidget();
+
+  private:
+
+};
 
 class Widget:public QWidget 
 { 
     Q_OBJECT public: 
-    Widget():m_label(new QLabel), m_button(new QPushButton("Button")), m_worker(new Worker) 
-    { 
-        QVBoxLayout * vbox = new QVBoxLayout(this); 
-        vbox->addWidget(m_label); 
-        vbox->addWidget(m_button); 
-        setLayout(vbox); 
-        thread = new QThread(this); 
-        m_worker->moveToThread(thread); 
-         connect(m_button,SIGNAL(clicked()),this,SLOT(onButtonClicked())); 
-        connect(this,SIGNAL(clicked(int)),m_worker,SLOT(slot1(int))); 
-        connect(m_worker,SIGNAL(sig1(QString)),m_label,SLOT(setText(QString))); 
-        thread->start(); 
-    } 
+    Widget();
     ~Widget()
     {
       qDebug() << "wait thread end" << objectName();
       thread->exit(0);
       while (thread->isFinished() == false);
       qDebug() << "delete thread";
+    #if 0
       delete thread;
+      #endif
     }
 signals: 
     void clicked(int v); 
@@ -54,9 +55,10 @@ private slots:
     } 
 private: 
     QLabel * m_label; 
-    QPushButton * m_button; 
+    QPushButton * m_button, *button_;
     Worker * m_worker; 
     QThread *thread;
+    MyWidget *my_widget_;
 };
 
 
