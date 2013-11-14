21de61ea8589296f73a293fd4b1577ffa179e1f4
descent <descent@weiya.com.tw>
2013-11-14 13:10:57 +0800

    new file:   cpp11_regex

---
+++ cpp11_regex/r.cpp
@@ -0,0 +1,10 @@
+#include <regex>
+
+//using namespace std;
+
+int main(int argc, char *argv[])
+{
+  std::regex self_regex("REGULAR EXPRESSIONS", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  
+  return 0;
+}
