d7d1dbafe706584fc5abc5dcb5bb1114c01e3a01
descent <descent0@gmail.com>
2013-11-18 17:07:43 +0800

    add another example

--- qstring_utf8/ex.sh
+++ qstring_utf8/ex.sh
@@ -1,3 +1,4 @@
 sudo ./main -p ./ukai.ttc -s 100 -t 龜 -b 0 -f 1 -g 1 -a 1
 sudo ./main -p ./ukai.ttc -s 150 -t B -b 0 -f 1 -g 1 -a 1
 
+./main -p ./mingliu.ttc -s 16 -m f3 -b   -f * -g 1 -a 0 -x 0 -y 16 -d 32
