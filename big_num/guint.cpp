b9f011be6b9b2593fca995b9700f21e5420f1603
dd <xyz@abc>
2013-12-27 14:25:54 +0800

    add operator<< for output

--- big_num/guint.cpp
+++ big_num/guint.cpp
@@ -2,12 +2,10 @@
 
 #include <cstdio>
 #include <string>
-#include <iostream>
 
-using namespace std;
 
-DS::Guint::Guint(const char *num)
-  : idx_(0)
+Guint::Guint(const std::string num)
+  : idx_(0), carry_(0)
 {
   string n(num);
   idx_ = 0;
@@ -26,4 +24,46 @@ DS::Guint::Guint(const char *num)
   sub_str = n.substr(0, i+5);
   //cout << "xx : " << sub_str << endl;
   data_[idx_] = strtol(sub_str.c_str(), NULL, 10);
+}
+
+Guint::Guint(const char *num)
+  : idx_(0), carry_(0)
+{
+  string n(num);
+  idx_ = 0;
+  string sub_str;
+  int i;
+  for (i=n.size()-5 ; i >= 0 ; i-=5)
+  {
+    cout << "i: " << i << endl;
+    string sub_str = n.substr(i, 5);
+    data_[idx_] = strtol(sub_str.c_str(), NULL, 10);
+    ++idx_;
+    //cout << "xx : " << sub_str << endl;
+  }
+
+  cout << "i: " << i << endl;
+  sub_str = n.substr(0, i+5);
+  //cout << "xx : " << sub_str << endl;
+  data_[idx_] = strtol(sub_str.c_str(), NULL, 10);
+}
+
+Guint Guint::add(const Guint &guint)
+{
+  for (int i=0 ; i < NUM ; ++i)
+  {
+
+
+  }
+}
+
+
+ostream& operator<<(ostream &os, const Guint &guint)
+{
+  for (int i=NUM-1 ; i >= 0 ; --i)
+  {
+    if (guint.data_[i] != 0)
+      os << guint.data_[i];
+  }
+  return os;
 }
--- big_num/guint.h
+++ big_num/guint.h
@@ -1,21 +1,31 @@
 #ifndef GUINT_H
 #define GUINT_H
 
+#include <iostream>
+using namespace std;
+
 #include "type.h"
 
 const int NUM=5;
 
-namespace DS
-{
+
+//namespace DS
+//{
 
 class Guint
 {
+  friend ostream & operator <<(ostream &os, const Guint &guint);
   public:
     Guint(const char *num);
+    Guint(const std::string num);
+    Guint add(const Guint &guint);
   private:
-    u16 data_[NUM];
     int idx_;
+    u16 carry_;
+    u16 data_[NUM]={0,0,0,0,0};
 };
 
-} //namespace DS
+//} //namespace DS
+
+//ostream & operator <<(ostream &os, const DS::Guint &guint);
 #endif
