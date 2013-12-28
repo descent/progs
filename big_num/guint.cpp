485a797df4342b05734ce5fc0edcc469c29e24f4
dd <xyz@abc>
2013-12-28 16:28:17 +0800

    add ctor(u32), operator++

--- big_num/guint.cpp
+++ big_num/guint.cpp
@@ -14,6 +14,19 @@ Guint::Guint(const vector<u16> data)
   data_ = data;
 }
 
+Guint::Guint(u32 num)
+  : idx_(0)
+{
+  u16 i1 = num & 0xffff;
+  u16 i2 = ((num >> 16 ) & 0xffff);
+  #if 0
+  cout << "i1: " << i1 << endl;
+  cout << "i2: " << i2 << endl;
+  #endif
+  data_.push_back(i1);
+  data_.push_back(i2);
+}
+
 Guint::Guint(const std::string num)
   : idx_(0)
 {
@@ -73,16 +86,21 @@ bool Guint::convert_to_cal_data(const std::string num)
     int j = nstr[k-1] - '0';
     data_.push_back(j*10 + i);
   }
-
+#if 0
   cout << "number" << endl;
 
   for (int i=0 ; i < data_.size() ; ++i)
   {
     cout << data_[i] << endl;
   }
-
+#endif
   return true;
+}
 
+Guint Guint::add(u32 n) const
+{
+  Guint a(n);
+  return add(a);
 }
 
 Guint Guint::add(const Guint &guint) const
@@ -92,15 +110,17 @@ Guint Guint::add(const Guint &guint) const
 
   int n = max(count(), guint.count());
 
-  cout << "n: " << n << endl;
+  //cout << "n: " << n << endl;
 
   for (int i=0 ; i < n ; ++i)
   {
     u16 a1 = i < count() ? data_[i] : 0;
     u16 a2 = i < guint.count() ? guint.data_[i] : 0;
     u32 t = a1 + a2  + carry;
+#if 0
     cout << "a1: " << a1 << endl;
     cout << "a2: " << a2 << endl;
+#endif
     //stringstream ss;
     //ss << (t%100);
     //string tmp = ss.str();
@@ -108,18 +128,43 @@ Guint Guint::add(const Guint &guint) const
     data.push_back(t%100);
     carry = t/100;
   }
-
+#if 0
   for (int i=0 ; i < data.size() ; ++i)
   {
     cout << data[i] << endl;
   }
+#endif
   return Guint(data);
+}
+
+// prefix ++
+Guint Guint::operator++()
+{
+  cout << "prefix++" << endl;
+  *this = add(1);
+  return (*this);
+}
+
+// postfix ++
+Guint Guint::operator++(int)
+{
+  cout << "postfix++" << endl;
+  Guint tmp(*this);
+  *this = add(1);
+
+  return tmp;
 }
 
 Guint operator+(const Guint &guint1, const Guint &guint2)
 {
   return guint1.add(guint2);
 }
+
+Guint operator+(const Guint &guint1, u32 n)
+{
+  return guint1.add(n);
+}
+
 
 
 ostream& operator<<(ostream &os, const Guint &guint)
--- big_num/guint.h
+++ big_num/guint.h
@@ -21,12 +21,17 @@ class Guint
 {
   friend ostream & operator <<(ostream &os, const Guint &guint);
   friend Guint operator+(const Guint &guint1, const Guint &guint2);
+  friend Guint operator+(const Guint &guint1, u32 n);
   public:
     Guint();
+    Guint(u32 num);
     Guint(const char *num);
     Guint(const std::string num);
     Guint(const vector<u16> data);
     Guint add(const Guint &guint) const;
+    Guint add(u32 n) const;
+    Guint operator++();
+    Guint operator++(int);
   private:
     int count() const {return data_.size();}
     bool convert_to_cal_data(const std::string num);
