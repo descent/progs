894aabda85a14b314a131caef6c51d86fe3c3cde
descent <descent0@gmail.com>
2014-12-18 14:38:16 +0800

    complete del(), search()

--- linked_list/list.c
+++ linked_list/list.c
@@ -36,10 +36,41 @@ void push_back(Node *node)
 
 void del(Node *node)
 {
+  if (node == head)
+  {
+    printf("del head\n");
+    head = node->next;
+    free(node);
+  }
+  else
+  {
+    Node *cur = head;
+    Node *previous_node = 0;
+    printf("del no head\n");
+    while (cur != 0)
+    {
+      if (cur == node)
+      {
+        break;
+      }
+      previous_node = cur;
+      cur = cur->next;
+    }
+    previous_node->next = node->next;
+    free(node);
+  }
 }
 
-void search(NodeElm elm)
+Node* search(const NodeElm *elm)
 {
+  Node *cur = head; 
+
+  for (; cur != 0 ; cur = cur->next)
+  {
+    if (*elm == cur->elm)
+      return cur;
+  }
+  return 0;
 }
 
 void print_list()
@@ -57,6 +88,25 @@ int main(int argc, char *argv[])
   push_back(make_node(1));
   push_back(make_node(2));
   push_back(make_node(3));
+  push_back(make_node(4));
+  push_back(make_node(5));
   print_list();
+
+  //NodeElm elm=1; // head
+  //NodeElm elm=2; // middle
+  NodeElm elm=5;   // tail
+  Node *n = search(&elm);
+
+  if (n)
+  {
+    printf("find n->elm: %d\n", n->elm);
+    del(n);
+  }
+  else
+  {
+    printf("not found\n");
+  }
+  print_list();
+
   return 0;
 }
