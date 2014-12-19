23ba29998dbfede4aad8ceb581a501e3e313d3d1
descent <descent0@gmail.com>
2014-12-19 15:55:24 +0800

    add partition for quick sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -8,6 +8,46 @@ using namespace std;
   char str[] = unsort; \
   int len = strlen(str); 
 
+void print_array(char *s, int len)
+{
+  for (int i=0 ; i < len ; ++i)
+    printf("%c ", s[i]);
+  printf("\n");
+}
+
+int partition(char *s, int begin, int end)
+{
+  char left[100];
+  char right[100];
+  int left_index=0;
+  int right_index=0;
+  int len = end - begin + 1;
+  int pivot_index = len/2;
+  char pivot = s[len/2];
+
+  print_array(s, len);
+  printf("s[%d] = pivot: %c\n", pivot_index, pivot);
+
+  for (int i=0 ; i < len ; ++i)
+  {
+    if (i == pivot_index)
+      continue;
+    if (s[i] < pivot)
+      left[left_index++] = s[i];
+    else
+      right[right_index++] = s[i];
+  }
+
+  print_array(left, left_index);
+  printf("\n");
+  print_array(right, right_index);
+  memcpy(s, left, left_index);
+  s[left_index] = pivot;
+  memcpy(s+left_index+1, right, right_index);
+
+  return left_index;
+}
+
 void insertion_sort()
 {
   DATA("98765")
@@ -60,6 +100,30 @@ int main(int argc, char *argv[])
 {
   bubble_sort();
   insertion_sort();
+
+  DATA("986521374")
+  int pivot_index = partition(str, 0, len-1);
+  printf("new pivot_index: %d\n", pivot_index);
+  print_array(str, len);
+  printf("===\n");
+
+  if ((pivot_index - 0) > 1)
+  {
+    printf("left\n");
+    pivot_index = partition(str, 0, pivot_index-1);
+    printf("new pivot_index: %d\n", pivot_index);
+  }
+
+  if ((len - 1 - pivot_index) > 1)
+  {
+    printf("right\n");
+    pivot_index = partition(str+pivot_index+1, pivot_index+1, len-1);
+    printf("new pivot_index: %d\n", pivot_index);
+  }
+  printf("===\n");
+  print_array(str, len);
+  printf("===\n");
+
 
   return 0;
 }
