03671f78cc1ff48bbf3d18025dd2e8542f1b1dc2
descent <descent0@gmail.com>
2015-01-07 14:44:30 +0800

    add send message, and receive the message which the server returns.

--- socket/uds_c.cpp
+++ socket/uds_c.cpp
@@ -2,7 +2,9 @@
 #include <sys/types.h>          /* See NOTES */
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include "common.h"
 
@@ -16,16 +18,40 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  struct sockaddr_un my_addr;
+  struct sockaddr_un my_addr, server_addr;
+  char my_path[]="/tmp/cli-XXXXXX";
+  //char *my_path;
+
+  mktemp(my_path);
+  PF("my_path: %s\n", my_path);
+  //return 0;
 
   my_addr.sun_family = AF_UNIX;
-  strncpy(my_addr.sun_path, UDS_ADDR, sizeof(my_addr.sun_path) - 1);
+  strncpy(my_addr.sun_path, my_path, sizeof(my_addr.sun_path) - 1);
+  PF("my_addr.sun_path: %s\n", my_addr.sun_path);
 
-  socklen_t addr_len = sizeof(my_addr);
+#if 1
+  //socklen_t addr_len = sizeof(my_addr);
+  int ret = bind(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
+
+  if (ret == -1)
+  {
+    perror("bind()");
+    return -1;
+  }
+#endif
+
 
   //ssize_t sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
-  char buf[100]="abc test";
-  ssize_t send_len = sendto(sockfd, buf, strlen(buf), 0, (sockaddr *)&my_addr, addr_len);
+  char buf[MAX_MESG_SIZE]="abc test";
+
+  if (argc > 1)
+    strncpy(buf, argv[1], 99);
+
+  server_addr.sun_family = AF_UNIX;
+  strncpy(server_addr.sun_path, UDS_ADDR, sizeof(server_addr.sun_path) - 1);
+
+  ssize_t send_len = sendto(sockfd, buf, strlen(buf), 0, (sockaddr *)&server_addr, sizeof(server_addr));
   if (send_len == -1)
   {
     perror("sendto");
@@ -33,5 +59,13 @@ int main(int argc, char *argv[])
 
   PF("send_len: %d\n", send_len);
   
+  socklen_t sz=100;
+
+  int read_size = recvfrom(sockfd, buf, MAX_MESG_SIZE, 0, NULL, &sz);
+  PF("read_size: %d\n", read_size);
+  PF("buf: %s\n", buf);
+
+  unlink(my_path);
+
   return 0;
 }
