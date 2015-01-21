094f1f407559a7fe4541635bb84047545b23555c
descent <descent0@gmail.com>
2015-01-21 17:07:48 +0800

    directly pass lambda function, need not declare std::function variable

--- lambda/t.cpp
+++ lambda/t.cpp
@@ -25,40 +25,45 @@ void extract(const string &str, std::function<void(const string&, const string&)
     // std::function<int(int ,int)>func=add;
     //   <int(int,int)>是实例化模板参数,表示返回值为int,函数参数为2个,(int,int),
     //   即int(*pfunc)(int ,int )类型的函数
-    std::function<void(const string&, const string&)> f1 = [&](const string &n, const string &s)
-    { 
-      char next_ch = str[0];
-      string tmp_str;
 
-      tmp_str.push_back(next_ch);
+    extract
+    (
+      str.substr(1), 
+      [&](const string &n, const string &s)
+      { 
+        char next_ch = str[0];
+        string tmp_str;
 
-#ifdef MORE_INFO
-      cout << "n: " << n << endl;
-      cout << "s: " << s << endl;
-      cout << "next_ch: " << next_ch << endl;
-#endif
+        tmp_str.push_back(next_ch);
 
-      if ('0' <= next_ch && next_ch <= '9')
-      {
-        string new_str = n + tmp_str;
-#ifdef MORE_INFO
-        cout << "new_str: " << new_str << endl;
-        cout << "===========" << endl;
-#endif
-        receive(new_str, s);
+  #ifdef MORE_INFO
+        cout << "n: " << n << endl;
+        cout << "s: " << s << endl;
+        cout << "next_ch: " << next_ch << endl;
+  #endif
+
+        if ('0' <= next_ch && next_ch <= '9')
+        {
+          string new_str = n + tmp_str;
+  #ifdef MORE_INFO
+          cout << "new_str: " << new_str << endl;
+          cout << "===========" << endl;
+  #endif
+          receive(new_str, s);
+        }
+        else
+        {
+          string new_str = s + tmp_str;
+  #ifdef MORE_INFO
+          cout << "new_str: " << new_str << endl;
+          cout << "===========" << endl;
+  #endif
+          receive(n, new_str);
+        }
       }
-      else
-      {
-        string new_str = s + tmp_str;
-#ifdef MORE_INFO
-        cout << "new_str: " << new_str << endl;
-        cout << "===========" << endl;
-#endif
-        receive(n, new_str);
-      }
-    };
-
-    extract(str.substr(1), f1);
+    
+    
+    );
   }
 }
 
