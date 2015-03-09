2aff37aaf1927e12c55f265fed3fdf050f6eba4e
descent <descent0@gmail.com>
2015-03-09 10:10:55 +0800

    add a example

--- cpp11_utf8/ex.sh
+++ cpp11_utf8/ex.sh
@@ -1,4 +1,7 @@
+# chinese text must be utf8 encoding.
+
 sudo ./main -p ./ukai.ttc -s 100 -t 龜 -b 0 -f 1 -g 1 -a 1
 sudo ./main -p ./ukai.ttc -s 150 -t B -b 0 -f 1 -g 1 -a 1
 
 ./main -p ./mingliu.ttc -s 16 -m f3 -b ' ' -f '*' -g 1 -a 0 -x 0 -y 16 -d 32
+./main -p ./mingliu.ttc -t 中 -s 14
