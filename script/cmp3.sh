0cd5b6723901bf86b50717e2ca71e926b61c5791
descent <descent0@gmail.com>
2015-05-23 23:59:54 +0800

    add get filename and extension

--- script/cmp3.sh
+++ script/cmp3.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
-
+# ref: http://ephrain.pixnet.net/blog/post/58136247-%5Blinux%5D-%E4%BD%BF%E7%94%A8-shell-%E6%88%AA%E5%8F%96%E6%AA%94%E6%A1%88%E7%9A%84%E6%AA%94%E5%90%8D%E8%88%87%E5%89%AF%E6%AA%94%E5%90%8D
 org_fn=$1
 fn=`ls "$org_fn"  |cut -d '.' -f1`
+filename="${org_fn%.*}"
+extension="${org_fn##*.}"
 echo $fn
+echo $filename
+echo $extension
 
-echo ffmpeg -vn -i "\"$org_fn\"" -ar 44100 -ab 128k "\"$fn.mp3\""
-echo avconv -vn -i "\"$org_fn\"" -ar 44100 -ab 128k "\"$fn.mp3\""
-avconv -vn -i "$org_fn" -ar 44100 -ab 128k "$fn.mp3"
+echo ffmpeg -vn -i "\"$org_fn\"" -ar 44100 -ab 128k "\"$filename.mp3\""
+ffmpeg -vn -i "\"$org_fn\"" -ar 44100 -ab 128k "\"$filename.mp3\""
+#echo avconv -vn -i "\"$org_fn\"" -ar 44100 -ab 128k "\"$fn.mp3\""
+#avconv -vn -i "$org_fn" -ar 44100 -ab 128k "$fn.mp3"
