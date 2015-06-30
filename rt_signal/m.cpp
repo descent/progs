24d3d6cff370d34da0c1a061d31ecafac33856da
descent <descent0@gmail.com>
2015-06-30 10:45:52 +0800

    test RTMIN+2

--- rt_signal/m.cpp
+++ rt_signal/m.cpp
@@ -1,9 +1,11 @@
 /*
  kill -s SIGRTMIN 2519
+ kill  -RTMIN+2 2519
  kill -s SIGRTMAX 2519
 
-  sig : 34
-  sig : 64
+  sig : 34 // SIGRTMIN
+  sig : 36 // RTMIN+2
+  sig : 64 // SIGRTMAX
  */ 
 
 #include <signal.h>
@@ -17,6 +19,7 @@ void sig_handler(int sig)
 int main(int argc, char *argv[])
 {
   signal(SIGRTMIN, sig_handler);
+  signal(SIGRTMIN+2, sig_handler);
   signal(SIGRTMAX, sig_handler);
 
   while(1)
