d49f067ed0950a932816b12710cd02ceadb7eefd
descent <descent0@gmail.com>
2015-07-24 19:19:14 +0800

    add more test code

--- cpp11_utf8/draw_test.cpp
+++ cpp11_utf8/draw_test.cpp
@@ -17,6 +17,8 @@ void segfault_sigaction(int signal, siginfo_t *si, void *arg)
 
 int main(int argc, char *argv[])
 {
+  int pen_x = 0;
+  int pen_y = 16;
 
   struct sigaction sa;
 
@@ -34,10 +36,36 @@ int main(int argc, char *argv[])
 
   FT_GlyphSlot slot;
 
-  std::wstring utf32_str = utf8_to_wstring("A中");
+  std::wstring utf32_str = utf8_to_wstring("A中B");
 
-  ft2.get_slot(slot, utf32_str[1]);
+  ft2.get_slot(slot, utf32_str[0]);
   my_draw_bitmap_mono(&slot->bitmap, 0, 0);
 
+  int w = slot->bitmap.width;
+
+  ft2.get_slot(slot, utf32_str[1]);
+  my_draw_bitmap_mono(&slot->bitmap, w, 0);
+  w += slot->bitmap.width;
+
+  ft2.get_slot(slot, utf32_str[2]);
+  my_draw_bitmap_mono(&slot->bitmap, w, 0);
+  w += slot->bitmap.width;
+
+  for (int i=0 ; i < 25 ; ++i)
+  {
+
+  ft2.get_slot(slot, utf32_str[0]);
+  my_draw_bitmap_mono(&slot->bitmap, 0, pen_y*i);
+
+  w = slot->bitmap.width;
+
+  ft2.get_slot(slot, utf32_str[1]);
+  my_draw_bitmap_mono(&slot->bitmap, w, pen_y*i);
+  w += slot->bitmap.width;
+
+  ft2.get_slot(slot, utf32_str[2]);
+  my_draw_bitmap_mono(&slot->bitmap, w, pen_y*i);
+  w += slot->bitmap.width;
+  }
   return 0;
 }
