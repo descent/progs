ab530fe5d84cba413dcbd38baeedbd355fa819ff
descent <descent0@gmail.com>
2015-08-06 12:11:32 +0800

    add static inline

--- cpp11_utf8/wstring2utf8.h
+++ cpp11_utf8/wstring2utf8.h
@@ -14,14 +14,14 @@
 #include <locale>
 
 // convert UTF-8 string to wstring
-std::wstring utf8_to_wstring (const std::string& str)
+static inline std::wstring utf8_to_wstring (const std::string& str)
 {
     std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
     return myconv.from_bytes(str);
 }
 
 // convert wstring to UTF-8 string
-std::string wstring_to_utf8 (const std::wstring& str)
+static inline std::string wstring_to_utf8 (const std::wstring& str)
 {
     std::wstring_convert<std::codecvt_utf8<wchar_t>> myconv;
     return myconv.to_bytes(str);
