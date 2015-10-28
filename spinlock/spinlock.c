71737942c731da8fe2d828da4243200d3c43e1e9
descent <descent0@gmail.com>
2015-10-28 14:03:52 +0800

    add pthread spinlock func to test

--- spinlock/spinlock.c
+++ spinlock/spinlock.c
@@ -42,6 +42,7 @@ void spinlock_init(Spinlock *spinlock)
 int spin_lock(Spinlock *spinlock)
 {
   atomic_sub(1, spinlock);
+  //--spinlock->val_;
   while(spinlock->val_ < 0);
 }
 
@@ -57,6 +58,7 @@ int spin_unlock(Spinlock *spinlock)
 
 FILE *fs;
 Spinlock sp;
+pthread_spinlock_t spinlock;
 
 int run=1;
 
@@ -66,9 +68,19 @@ void* write_file_1(void *p)
 
   while(run)
   {
+#ifdef PTHREAD_FUNC
+    pthread_spin_lock(&spinlock);
+#else
     spin_lock(&sp);
+#endif
     fprintf(fs, "%d ## thread 1\n", time);
+    fprintf(fs, "%d ## thread 11\n", time);
+    fprintf(fs, "%d ## thread 111\n", time);
+#ifdef PTHREAD_FUNC
+    pthread_spin_unlock(&spinlock);
+#else
     spin_unlock(&sp);
+#endif
     ++time;
   }
   // printf("thread 1 exit\n");
@@ -80,9 +92,44 @@ void* write_file_2(void *p)
 
   while(run)
   {
+#ifdef PTHREAD_FUNC
+    pthread_spin_lock(&spinlock);
+#else
     spin_lock(&sp);
+#endif
     fprintf(fs, "%d ## thread 2 long string 0123456789\n", time);
+    fprintf(fs, "%d ## thread 22 long string 0123456789\n", time);
+    fprintf(fs, "%d ## thread 222 long string 0123456789\n", time);
+#ifdef PTHREAD_FUNC
+    pthread_spin_unlock(&spinlock);
+#else
     spin_unlock(&sp);
+#endif
+    ++time;
+  }
+  // printf("thread 2 exit\n");
+}
+
+void* write_file_3(void *p)
+{
+  int time = 0;
+  int i;
+
+  while(run)
+  {
+#ifdef PTHREAD_FUNC
+    pthread_spin_lock(&spinlock);
+#else
+    spin_lock(&sp);
+#endif
+    fprintf(fs, "%d ## thread 3 long string ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()~\n", time);
+    fprintf(fs, "%d ## thread 33 long string ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()~\n", time);
+    fprintf(fs, "%d ## thread 333 long string ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()~\n", time);
+#ifdef PTHREAD_FUNC
+    pthread_spin_unlock(&spinlock);
+#else
+    spin_unlock(&sp);
+#endif
     ++time;
   }
   // printf("thread 2 exit\n");
@@ -110,12 +157,20 @@ static void* sig_thread (void *arg)
   }
 }
 
-int ret1, ret2;
+int ret1, ret2, ret3;
 
 int main(int argc, char *argv[])
 {
-  pthread_t thread0, thread1, thread;
+  pthread_t thread0, thread1, thread2, thread;
   sigset_t set;
+
+#ifdef PTHREAD_FUNC
+  pthread_spin_init(&spinlock, 0);
+  printf("init pthread spinlock\n");
+#else
+  spinlock_init(&sp);
+  printf("init my spinlock\n");
+#endif
 
   sigemptyset (&set);
   sigaddset (&set, SIGQUIT);
@@ -127,7 +182,6 @@ int main(int argc, char *argv[])
     return -1;
   }
 
-  spinlock_init(&sp);
 
   fs = fopen("/tmp/xyz1", "w");
   if (fs == NULL) 
@@ -139,9 +193,12 @@ int main(int argc, char *argv[])
   pthread_create (&thread, NULL, &sig_thread, (void *) &set);
   pthread_create(&thread0, NULL, write_file_1, NULL);
   pthread_create(&thread1, NULL, write_file_2, NULL);
+  pthread_create(&thread2, NULL, write_file_3, NULL);
 
   pthread_join(thread0, &ret1);
   pthread_join(thread1, &ret2);
+  pthread_join(thread2, &ret3);
+
   fclose(fs);
   printf("test end\n");
   
--- spinlock/spinlock.h
+++ spinlock/spinlock.h
@@ -9,4 +9,32 @@ typedef struct
 
 typedef Atomic Spinlock;
 
+typedef unsigned int u32;
+typedef unsigned int u16;
+
+#define TICKET_SHIFT    16
+
+
+#define barrier() __asm__ __volatile__("": : :"memory")
+
+
+
+typedef struct {
+        union {
+                u32 lock;
+                struct __raw_tickets {
+#ifdef __ARMEB__
+                        u16 next;
+                        u16 owner;
+#else
+                        u16 owner;
+                        u16 next;
+#endif
+                } tickets;
+        };
+} arch_spinlock_t;
+
+
+// #define PTHREAD_FUNC
+
 #endif
