d5198af8ca963a06e536ad9695671d2e55a22a65
descent <descent0@gmail.com>
2015-11-03 15:37:13 +0800

    new file:   test.sh

---
+++ spinlock/test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+time=20
+
+if [ "$1" != "" ]; then
+last=$1
+fi
+
+i=0
+DURATION=1
+
+echo "$time run"
+while [ $i -le $time ];do
+DURATION=$[1 + $[ RANDOM % 10 ]]
+echo "$i ## DURATION: $DURATION =============="
+timeout -s SIGINT $DURATION ./spinlock
+ls -l /tmp/xyz1
+./check_result
+i=`expr $i + 1`
+done
+
+
