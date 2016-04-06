eabc8b0a9e35a842ee6033419bbfe1a8b8f5386a
descent <descent0@gmail.com>
2016-04-06 14:00:11 +0800

    new file:   java/Point2D.java 	new file:   java/UsePackage.java 	new file:   java/makefile 	new file:   java/readme           for test javac compiling environment.

---
+++ java/Point2D.java
@@ -0,0 +1,10 @@
+package onlyfun.caterpillar; 
+ 
+public class Point2D { 
+    private int x, y;
+ 
+    public Point2D() {x = 0; y = 0;} 
+    public Point2D(int x, int y) {this.x = x; this.y = y;} 
+    public int getX() {return x;} 
+    public int getY() {return y;} 
+} 
