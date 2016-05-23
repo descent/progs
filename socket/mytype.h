9a73b7f61e04c8ebe4960700c1091c8dba675f9f
descent <descent0@gmail.com>
2016-05-23 15:30:31 +0800

    new file:   mytype.h

---
+++ socket/mytype.h
@@ -0,0 +1,17 @@
+#ifndef MY_TYPE_H
+#define MY_TYPE_H
+
+#define OK 0
+#define ERR 1
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef unsigned long long u64;
+
+#endif
