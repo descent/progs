26a09f55fa7352bab387cef17a04d56966bc7128
descent <descent0@gmail.com>
2016-05-23 15:27:28 +0800

    merge send/recv

--- socket/raw_sock_test.cpp
+++ socket/raw_sock_test.cpp
@@ -5,9 +5,10 @@
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  */
-
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,6 +16,18 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <netinet/ether.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <net/if.h>
+#include <netinet/ether.h>
+
+#include <unistd.h>
+
+#include "mytype.h"
 
 #define MY_DEST_MAC0	0x00
 #define MY_DEST_MAC1	0x00
@@ -24,25 +37,53 @@
 #define MY_DEST_MAC5	0x00
 
 #define DEFAULT_IF	"eth0"
+
+#define DEST_MAC0	0x00
+#define DEST_MAC1	0x00
+#define DEST_MAC2	0x00
+#define DEST_MAC3	0x00
+#define DEST_MAC4	0x00
+#define DEST_MAC5	0x00
+
+#define ETHER_TYPE	0x0800
+
 #define BUF_SIZ		1024
 
-int main(int argc, char *argv[])
+u8 pdo_packet[60] = {0xff, 0xff, 0xff, 0xFF, 0xFF, 0xFF, 0x88, 0x88, 0x88, 0x88, 0x88, 0x87, 0x88, 0xA4, 0x0E, 0x10, 0x07, 0x01, 0x00, 0x00, 0x30, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+u8 sdo_packet[60] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x88, 0x88, 0x88, 0x88, 0x88, 0x87, 0x88, 0xA4, 0x0E, 0x10, 0x07, 0x80, 0x00, 0x00, 0x30, 0x01, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+u16 packet_size = 60;
+
+void print_packet(u8 *packet, u32 len)
+{
+  int i;
+
+  printf("len: %d\n", len);
+
+  for (i=0 ; i < len ; ++i)
+  {
+    if (i%16 == 0)
+      printf("\n");
+
+    printf("%02x ", packet[i]);
+  }
+  printf("\n");
+}
+
+int raw_send(const char *if_str)
 {
 	int sockfd;
 	struct ifreq if_idx;
 	struct ifreq if_mac;
 	int tx_len = 0;
-	char sendbuf[BUF_SIZ];
+	u8 sendbuf[BUF_SIZ];
 	struct ether_header *eh = (struct ether_header *) sendbuf;
 	struct iphdr *iph = (struct iphdr *) (sendbuf + sizeof(struct ether_header));
 	struct sockaddr_ll socket_address;
 	char ifName[IFNAMSIZ];
 	
 	/* Get interface name */
-	if (argc > 1)
-		strcpy(ifName, argv[1]);
-	else
-		strcpy(ifName, DEFAULT_IF);
+	strcpy(ifName, if_str);
+	//strcpy(ifName, DEFAULT_IF);
 
 	/* Open RAW socket to send on */
 	if ((sockfd = socket(AF_PACKET, SOCK_RAW, IPPROTO_RAW)) == -1) {
@@ -59,7 +100,7 @@ int main(int argc, char *argv[])
 	strncpy(if_mac.ifr_name, ifName, IFNAMSIZ-1);
 	if (ioctl(sockfd, SIOCGIFHWADDR, &if_mac) < 0)
 	    perror("SIOCGIFHWADDR");
-
+#if 1
 	/* Construct the Ethernet header */
 	memset(sendbuf, 0, BUF_SIZ);
 	/* Ethernet header */
@@ -69,12 +110,23 @@ int main(int argc, char *argv[])
 	eh->ether_shost[3] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[3];
 	eh->ether_shost[4] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[4];
 	eh->ether_shost[5] = ((uint8_t *)&if_mac.ifr_hwaddr.sa_data)[5];
+
 	eh->ether_dhost[0] = MY_DEST_MAC0;
 	eh->ether_dhost[1] = MY_DEST_MAC1;
 	eh->ether_dhost[2] = MY_DEST_MAC2;
 	eh->ether_dhost[3] = MY_DEST_MAC3;
 	eh->ether_dhost[4] = MY_DEST_MAC4;
 	eh->ether_dhost[5] = MY_DEST_MAC5;
+
+
+	eh->ether_dhost[0] = 0xff;
+	eh->ether_dhost[1] = 0xff;
+	eh->ether_dhost[2] = 0xff;
+	eh->ether_dhost[3] = 0xff;
+	eh->ether_dhost[4] = 0xff;
+	eh->ether_dhost[5] = 0xff;
+
+
 	/* Ethertype field */
 	eh->ether_type = htons(ETH_P_IP);
 	tx_len += sizeof(struct ether_header);
@@ -84,6 +136,7 @@ int main(int argc, char *argv[])
 	sendbuf[tx_len++] = 0xad;
 	sendbuf[tx_len++] = 0xbe;
 	sendbuf[tx_len++] = 0xef;
+  memcpy(sendbuf, pdo_packet, 60);
 
 	/* Index of the network device */
 	socket_address.sll_ifindex = if_idx.ifr_ifindex;
@@ -96,10 +149,163 @@ int main(int argc, char *argv[])
 	socket_address.sll_addr[3] = MY_DEST_MAC3;
 	socket_address.sll_addr[4] = MY_DEST_MAC4;
 	socket_address.sll_addr[5] = MY_DEST_MAC5;
-
+#endif
 	/* Send packet */
+        tx_len=60;
+  print_packet(sendbuf, tx_len);
 	if (sendto(sockfd, sendbuf, tx_len, 0, (struct sockaddr*)&socket_address, sizeof(struct sockaddr_ll)) < 0)
-	    printf("Send failed\n");
+        {
+          perror("send fail: ");
+	    // printf("Send failed\n");
+        }
 
 	return 0;
+}
+
+
+
+// ref: https://gist.github.com/austinmarton/2862515
+int promisc_recv(const char *if_str)
+{
+	char sender[INET6_ADDRSTRLEN];
+	int sockfd, ret, i;
+	int sockopt;
+	ssize_t numbytes;
+	struct ifreq ifopts;	/* set promiscuous mode */
+	struct ifreq if_ip;	/* get ip addr */
+	struct sockaddr_storage their_addr;
+	uint8_t buf[BUF_SIZ];
+	char ifName[IFNAMSIZ];
+	
+	/* Get interface name */
+        strcpy(ifName, if_str);
+	//strcpy(ifName, DEFAULT_IF);
+
+	/* Header structures */
+	struct ether_header *eh = (struct ether_header *) buf;
+	struct iphdr *iph = (struct iphdr *) (buf + sizeof(struct ether_header));
+	struct udphdr *udph = (struct udphdr *) (buf + sizeof(struct iphdr) + sizeof(struct ether_header));
+
+	memset(&if_ip, 0, sizeof(struct ifreq));
+
+	/* Open PF_PACKET socket, listening for EtherType ETHER_TYPE */
+	if ((sockfd = socket(PF_PACKET, SOCK_RAW, htons(ETHER_TYPE))) == -1) {
+		perror("listener: socket");	
+		return -1;
+	}
+
+#if 1
+	/* Set interface to promiscuous mode - do we need to do this every time? */
+	strncpy(ifopts.ifr_name, ifName, IFNAMSIZ-1);
+	ioctl(sockfd, SIOCGIFFLAGS, &ifopts);
+	ifopts.ifr_flags |= IFF_PROMISC;
+	ioctl(sockfd, SIOCSIFFLAGS, &ifopts);
+#endif
+	/* Allow the socket to be reused - incase connection is closed prematurely */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &sockopt, sizeof sockopt) == -1) {
+		perror("setsockopt");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+	/* Bind to device */
+	if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE, ifName, IFNAMSIZ-1) == -1)	{
+		perror("SO_BINDTODEVICE");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+
+
+  printf("listener: Waiting to recvfrom...\n");
+  fd_set rfds;
+  struct timeval tv;
+  int retval;
+
+  /* Watch stdin (fd 0) to see when it has input. */
+  FD_ZERO(&rfds);
+  FD_SET(sockfd, &rfds);
+
+  /* Wait up to five seconds. */
+  tv.tv_sec = 5;
+  tv.tv_usec = 0;
+
+  while(1)
+  {
+  retval = select(sockfd+1, &rfds, NULL, NULL, &tv);
+
+  if (retval == -1)
+      perror("select()");
+  else if (retval)
+       {
+         printf("Data is available now.\n");
+         numbytes = recvfrom(sockfd, buf, BUF_SIZ, 0, NULL, NULL);
+         printf("numbytes: %d\n", numbytes);
+         print_packet(buf, numbytes);
+         // FD_ISSET(0, &rfds) will be true. *
+       }
+       else
+         printf("No data within five seconds.\n");
+  }
+#if 0
+repeat:	printf("listener: Waiting to recvfrom...\n");
+	numbytes = recvfrom(sockfd, buf, BUF_SIZ, 0, NULL, NULL);
+	printf("listener: got packet %lu bytes\n", numbytes);
+
+	/* Check the packet is for me */
+	if (eh->ether_dhost[0] == DEST_MAC0 &&
+			eh->ether_dhost[1] == DEST_MAC1 &&
+			eh->ether_dhost[2] == DEST_MAC2 &&
+			eh->ether_dhost[3] == DEST_MAC3 &&
+			eh->ether_dhost[4] == DEST_MAC4 &&
+			eh->ether_dhost[5] == DEST_MAC5) {
+		printf("Correct destination MAC address\n");
+	} else {
+		printf("Wrong destination MAC: %x:%x:%x:%x:%x:%x\n",
+						eh->ether_dhost[0],
+						eh->ether_dhost[1],
+						eh->ether_dhost[2],
+						eh->ether_dhost[3],
+						eh->ether_dhost[4],
+						eh->ether_dhost[5]);
+		ret = -1;
+		goto done;
+	}
+
+	/* Get source IP */
+	((struct sockaddr_in *)&their_addr)->sin_addr.s_addr = iph->saddr;
+	inet_ntop(AF_INET, &((struct sockaddr_in*)&their_addr)->sin_addr, sender, sizeof sender);
+
+	/* Look up my device IP addr if possible */
+	strncpy(if_ip.ifr_name, ifName, IFNAMSIZ-1);
+	if (ioctl(sockfd, SIOCGIFADDR, &if_ip) >= 0) { /* if we can't check then don't */
+		printf("Source IP: %s\n My IP: %s\n", sender, 
+				inet_ntoa(((struct sockaddr_in *)&if_ip.ifr_addr)->sin_addr));
+		/* ignore if I sent it */
+		if (strcmp(sender, inet_ntoa(((struct sockaddr_in *)&if_ip.ifr_addr)->sin_addr)) == 0)	{
+			printf("but I sent it :(\n");
+			ret = -1;
+			goto done;
+		}
+	}
+
+	/* UDP payload length */
+	ret = ntohs(udph->len) - sizeof(struct udphdr);
+
+	/* Print packet */
+	printf("\tData:");
+	for (i=0; i<numbytes; i++) printf("%02x:", buf[i]);
+	printf("\n");
+
+done:	goto repeat;
+#endif
+	close(sockfd);
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+  printf("DEFAULT_IF: %s\n", DEFAULT_IF);
+  //raw_send("eth1");
+  promisc_recv("eth0");
+  
+  return 0;
 }
