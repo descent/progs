24aaed8ceb4d2215345dbd311d8b6fdb6af0ff0f
descent <descent0@gmail.com>
2016-07-04 13:10:49 +0800

    new file:   mytype.h

---
+++ mytype.h
@@ -0,0 +1,14 @@
+#ifndef MY_TYPE_H
+#define MY_TYPE_H
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed int s32;
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+typedef unsigned long long u64;
+
+#endif
