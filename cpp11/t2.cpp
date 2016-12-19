e05583f5888e431afab6587e9b40e5c6d8091253
descent <descent0@gmail.com>
2016-12-19 18:24:25 +0800

    modified:   makefile 	new file:   t2.cpp

---
+++ cpp11/t2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <string>
+
+using namespace std;
+using namespace std::chrono;
+
+#include <unistd.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+  //duration<int, ratio<1, 1000>> d{2};
+  //cout << d.count() << endl;
+  time_point<system_clock> epoch = time_point<system_clock>{};
+  time_point<system_clock> tp = system_clock::now();
+
+  time_point<steady_clock> tp1 = steady_clock::now();
+  //duration<int, ratio<1,1000000>> d = duration_cast<microseconds>(epoch - tp);
+  //duration<int, ratio<1,1000000>> d = duration_cast<microseconds>(tp - epoch);
+ 
+  typedef duration<unsigned long long, ratio<1,1000000>> unit;
+  //typedef duration<int, ratio<1,1000000>> unit;
+  //typedef microseconds unit;
+  unit d = duration_cast<unit>(tp - epoch);
+  cout << d.count() << endl;
+
+  return 0;
+}
