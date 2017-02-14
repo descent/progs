f55897eb72534107d6aac2a9ec53d06224013451
descent <descent0@gmail.com>
2017-02-14 16:31:01 +0800

    change algorithm

--- leetcode/412.c
+++ leetcode/412.c
@@ -14,43 +14,75 @@ char** fizzBuzz(int n, int* returnSize)
   #if 0
   static char *str_arr[3];
   #else
-  static char **str_arr;
+  char **str_arr;
   str_arr = malloc(n * (sizeof (char *)));
   #endif
   //char str[100];
+  const char *fb = "FizzBuzz";
+  const char *f = "Fizz";
+  const char *b = "Buzz";
+  const char *gen_str[] = {
+                            "FizzBuzz",
+                            "1",
+                            "2",
+                            "Fizz",
+                            "4",
+                            "Buzz",
+                            "Fizz",
+                            "7",
+                            "8",
+                            "Fizz",
+                            "Buzz",
+                            "11",
+                            "Fizz",
+                            "13",
+                            "14",
+                            "FizzBuzz",
+                          };
+
 
   //printf("str_arr %p, &str_arr %p\n", str_arr, &str_arr);
   for (int i = 1 ; i <= n ; ++i)
   {
-    if (i%3==0 && i%5==0)
+    char *num_str = malloc(20 * sizeof(char));
+
+    if (i<=15)
+      str_arr[i-1] = gen_str[i%15];
+    else
     {
-      str_arr[i-1] = strdup("FizzBuzz");
+
+    //if (i%3==0 && i%5==0)
+    if (i%15 == 0)
+    {
+      str_arr[i-1] = fb;
     }
     else if (i%3==0)
          {
-           str_arr[i-1] = strdup("Fizz");
+           str_arr[i-1] = f;
          }
          else if (i%5==0)
               {
-                str_arr[i-1] = strdup("Buzz");
+                str_arr[i-1] = b;
               }
               else
               {
-                char num_str[20]; 
           
                 sprintf(num_str, "%d", i);
-                str_arr[i-1] = strdup(num_str);
+                str_arr[i-1] = num_str;
               }
+               
+    }
     //printf("yy %s, &str_arr[i]: %p, str_arr[i]: %p\n", str_arr[i], &str_arr[i], str_arr[i]);
   }
   *returnSize = n;
   return str_arr; 
 }
 
+
 int main(int argc, char *argv[])
 {
   int ret_size;
-  int n=15;
+  int n=5;
   char **str_arr = fizzBuzz(n, &ret_size);
   printf("ret_size: %d\n", ret_size);
   for (int i=0 ; i < ret_size ; ++i)
