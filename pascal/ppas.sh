d6391b4dd129ac2db46d49c4957fc8142c4d1b50
descent <descent0@gmail.com>
2017-03-27 14:51:08 +0800

    for build an elf execute file.

---
+++ pascal/ppas.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DoExitAsm ()
+{ echo "An error occurred while assembling $1"; exit 1; }
+DoExitLink ()
+{ echo "An error occurred while linking $1"; exit 1; }
+echo Linking h
+OFS=$IFS
+IFS="
+"
+/usr/bin/ld -b elf32-i386 -m elf_i386     -s -L. -o h -T link.res -e _start
+if [ $? != 0 ]; then DoExitLink h; fi
+IFS=$OFS
